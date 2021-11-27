b2e5d2de10cd9e84f9626a3c84e6963e042fbd24
colderry <colderryz@gmail.com>
2021-11-27 18:12:57 +0600

    Discords [index] - properties

--- lib/Discords/index.ts
+++ lib/Discords/index.ts
@@ -1,10 +1,11 @@
-import Axios from "axios";
+import { User } from "./structures/User";
 import { Bot } from "./structures/Bot";
 
 export class Discords {
     public apiKey: string;
     public apiUrl: string;
     public bots: Bot;
+    public users: User;
 
     constructor (opt: DiscordsOptions) {
         /**
@@ -20,10 +21,16 @@ export class Discords {
         this.apiUrl = opt.apiUrl ? opt.apiUrl : "https://discords.com/bots/api";
         
         /**
-         * The bot class to interact with the bot endpoint
+         * The bot class to interact with the bot endpoints
          * @type {Bot}
          */
         this.bots = new Bot(this);
+
+        /**
+         * The user class to interact with the user endpoints
+         * @type {User}
+         */
+        this.users = new User(this);
     };
 };
 
